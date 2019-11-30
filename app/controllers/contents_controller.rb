ffdbd2697c8f22e78f4f097fd0b2206e9d3cd9c4
shozzy <shozzy.github@gmail.com>
2019-11-30 20:24:59 +0900

    add auth to contents

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,4 +1,5 @@
 class ContentsController < ApplicationController
+  before_action :authenticate_user!, only: [:show, :new, :edit, :create, :update, :destroy]
   before_action :set_content, only: [:show, :edit, :update, :destroy]
 
   # GET /contents
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
+  scope '/api' do
+    get '/contents', to: 'contents#index', defaults: { format: :json }
+    get '/contents/:id', to: 'contents#show', defaults: { format: :json }
+  end
 end
