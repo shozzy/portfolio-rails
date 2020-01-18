5ef6789e255564d9020341341a828f1f23c2e9f0
shozzy <shozzy.github@gmail.com>
2020-01-18 14:31:41 +0900

    add cors config

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module PortfolioRails
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.permitted_origin = ENV.fetch('PERMITTED_ORIGIN', 'http://localhost:8000')
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
